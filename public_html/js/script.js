077ade62d84966d1b84da767a01648425cb93017
bardius <george@bardis.info>
2016-02-03 21:31:59 +0000

    added contact page with map functionality

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -15,8 +15,8 @@
 
     BARDIS.Config = {
         $body: $(document.body),
-        init: function() {
 
+        init: function() {
             BARDIS.windowResize.init();
             BARDIS.foundation.init();
             BARDIS.UI.init();
@@ -36,17 +36,24 @@
         $sliderNavigationItems: $(".slider-navigation a"),
         $toggleTriggerItems: $(".togglerTrigger"),
         $scrollerTriggerItems: $(".scrollToTrigger"),
+
         init: function(){
             BARDIS.UI.sliderNavigationFix();
             BARDIS.UI.contentToggler();
             BARDIS.UI.contentScroller();
+
+            if($('#mapPage').length > 0){
+                BARDIS.mapConfig.mapInit();
+            }
         },
+
         sliderNavigationFix: function(){
             BARDIS.UI.$sliderNavigationItems.on("click", function(e){
                 BARDIS.UI.$sliderNavigationItems.removeClass("active");
                 $(this).addClass("active");
             });
         },
+
         contentToggler: function(){
             BARDIS.UI.$toggleTriggerItems.on("click", function(e){
                 e.preventDefault();
@@ -74,6 +81,7 @@
                 });
             });
         },
+
         contentScroller: function(){
             BARDIS.UI.$scrollerTriggerItems.on("click", function(e){
                 e.preventDefault();
@@ -98,18 +106,127 @@
                 }, 500);
             });
         }
+
+    };
+
+    BARDIS.mapConfig = {
+        // Map settings
+        map:			null,
+        mapKey:			'AIzaSyB8uoWbuhHNagbpi22tEeZYiT41toB171g',
+        mapLatitude:	$('#mapLat').val(),
+        mapLongitude:	$('#mapLong').val(),
+        mapZoom:		14,
+        markerTitle:	$('#mapTitle').val(),
+        mapCanvasId:	'map-canvas',
+        GMapScriptURL:	'http://maps.google.com/maps/api/js?sensor=false&key=',
+        map_html:		'',
+        map_to_html:	'',
+        map_from_html:	'',
+        map_infowindow:	'',
+        marker:			'',
+
+        mapInit: function() {
+            $.getScript(BARDIS.mapConfig.GMapScriptURL + BARDIS.mapConfig.mapKey + "&callback=BARDIS.mapConfig.showMap");
+        },
+
+        showMap: function() {
+            var windowHeight	= $(window).height();
+            var mapLatlng		= new google.maps.LatLng(BARDIS.mapConfig.mapLatitude, BARDIS.mapConfig.mapLongitude);
+            var contentString	= '<h4>' + BARDIS.mapConfig.markerTitle + '</h4>';
+
+            // The info window version with the "to here" form open
+            BARDIS.mapConfig.map_to_html = contentString + '<p>Get Directions: <b>To here</b> - <a href="javascript:BARDIS.mapConfig.mapFromHere()">From here</a></p>' +
+                '<form action="http://maps.google.com/maps" method="get"" target="_blank"><label for="saddr">Start address:</label>' +
+                '<input type="text" size="40" maxlength="80" name="saddr" id="saddr" value="" /><br />' +
+                '<input class="button small" value="Get Directions" type="submit" />' +
+                '<input type="hidden" name="daddr" value="' + BARDIS.mapConfig.mapLatitude + ',' + BARDIS.mapConfig.mapLongitude + "(" + BARDIS.mapConfig.markerTitle + ")" + '"/></form>';
+
+            // The info window version with the "to here" form open
+            BARDIS.mapConfig.map_from_html = contentString + '<p>Get Directions: <a href="javascript:BARDIS.mapConfig.mapToHere()">To here</a> - <b>From here</b></p>' +
+                '<form action="http://maps.google.com/maps" method="get"" target="_blank"><label for="daddr">End address:</label>' +
+                '<input type="text" size="40" maxlength="80" name="daddr" id="daddr" value="" /><br />' +
+                '<input class="button small" value="Get Directions" type="submit" />' +
+                '<input type="hidden" name="saddr" value="' + BARDIS.mapConfig.mapLatitude + ',' + BARDIS.mapConfig.mapLongitude + "(" + BARDIS.mapConfig.markerTitle + ")" + '"/></form>';
+
+            // The inactive version of the direction info
+            BARDIS.mapConfig.map_html = contentString + '<p>Get Directions: <a href="javascript:BARDIS.mapConfig.mapToHere()">To here</a> - <a href="javascript:BARDIS.mapConfig.mapFromHere()">From here</a></p>';
+
+            $('#' + BARDIS.mapConfig.mapCanvasId).css('height', (windowHeight / 2) + 'px');
+
+            if($('#mapZoom').length > 0){
+                BARDIS.mapConfig.mapZoom = parseInt($('#mapZoom').val());
+            }
+
+            var mapOptions = {
+                center:		mapLatlng,
+                mapTypeId:	google.maps.MapTypeId.ROADMAP,
+                zoom:		BARDIS.mapConfig.mapZoom
+            };
+
+            BARDIS.mapConfig.map = new google.maps.Map(document.getElementById(BARDIS.mapConfig.mapCanvasId), mapOptions);
+
+            google.maps.event.addDomListener(window, "resize", function() {
+                var newCenter = BARDIS.mapConfig.map.getCenter();
+                google.maps.event.trigger(BARDIS.mapConfig.map, "resize");
+                BARDIS.mapConfig.map.setCenter(newCenter);
+            });
+
+            if(BARDIS.mapConfig.mapZoom === 14){
+                BARDIS.mapConfig.marker	= new google.maps.Marker({
+                    position:	mapLatlng,
+                    map:		BARDIS.mapConfig.map,
+                    title:		BARDIS.mapConfig.markerTitle
+                });
+
+                BARDIS.mapConfig.map_infowindow = new google.maps.InfoWindow({
+                    content: BARDIS.mapConfig.map_html,
+                    disableAutoPan : false
+                });
+
+                google.maps.event.addListener(BARDIS.mapConfig.marker, 'click', function() {
+                    BARDIS.mapConfig.map_infowindow.open(BARDIS.mapConfig.map, BARDIS.mapConfig.marker);
+                });
+
+                google.maps.event.addListener(BARDIS.mapConfig.map_infowindow, 'closeclick', function() {
+                    BARDIS.mapConfig.map_infowindow.setContent(BARDIS.mapConfig.map_html);
+                });
+
+                //loaded fully
+                google.maps.event.addListenerOnce(BARDIS.mapConfig.map, 'idle', function(){
+                });
+            }
+
+            $(document).on('opened', '[data-reveal]', function () {
+                var newCenter = BARDIS.mapConfig.map.getCenter();
+                google.maps.event.trigger(BARDIS.mapConfig.map, "resize");
+                BARDIS.mapConfig.map.setCenter(newCenter);
+                BARDIS.mapConfig.map_infowindow.open(BARDIS.mapConfig.map, BARDIS.mapConfig.marker);
+            });
+        },
+
+        // functions that open the directions forms
+        mapToHere: function() {
+
+            BARDIS.mapConfig.map_infowindow.setContent(BARDIS.mapConfig.map_to_html);
+            BARDIS.mapConfig.map_infowindow.open(BARDIS.mapConfig.map, BARDIS.mapConfig.marker);
+        },
+
+        mapFromHere: function() {
+
+            BARDIS.mapConfig.map_infowindow.setContent(BARDIS.mapConfig.map_from_html);
+            BARDIS.mapConfig.map_infowindow.open(BARDIS.mapConfig.map, BARDIS.mapConfig.marker);
+        }
     };
 
     BARDIS.foundation = {
-        init: function() {
 
+        init: function() {
             $(document).foundation({
                 offcanvas: {
                     open_method: 'move', // Sets method in which offcanvas opens, can also be 'overlap'
                     close_on_click: true
                 },
                 equalizer : {
-
                     equalize_on_stack: false, // Specify if Equalizer should make elements equal height once they become stacked.
                     act_on_hidden_el: false // Allow equalizer to resize hidden elements
                 },
